c2c43c33d8ded0ccf174d5f4e6bb3c9916cd0227
Auralshin <41705919+auralshin@users.noreply.github.com>
2025-06-01 11:40:20 +0530

    feat: add custom CSS for sidebar logo and update configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,8 @@ exclude_patterns = []
 
 html_theme = 'furo'
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
+html_logo = "_static/logo.png"
 
 html_theme_options = {
     "light_css_variables": {
